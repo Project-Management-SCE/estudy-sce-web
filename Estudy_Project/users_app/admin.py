c3733c22f8d18e55f30073190cbe345228d47a74
RoniJackVituli <rjvtechnology@gmail.com>
2022-04-01 18:49:03 +0300

    learn about models

--- Estudy_Project/users_app/admin.py
+++ Estudy_Project/users_app/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from users_app.models import *
-# Register your models here.
 
-admin.site.register(AccessRecord)
-admin.site.register(Topic)
-admin.site.register(Webpage)
+# Register your models here.
