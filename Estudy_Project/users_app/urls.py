30d19234a02a7340ed39b49397109f50cfda7228
RoniJackVituli <rjvtechnology@gmail.com>
2022-04-01 10:41:03 +0300

    start over

--- Estudy_Project/users_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from users_app import views
-
-urlpatterns = [
-    path('test/', views.index, name='index')
-]
--- Estudy_Project/users_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def index(req):
-    my_dict = {'user_name': "Roni Jack Vituli", 'email': "Roni@gmail.com"}
-    return render(req, 'index.html', context=my_dict)
