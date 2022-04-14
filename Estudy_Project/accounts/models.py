ca34184d6ff2c8fd416f29c84ae777c28219415d
RoniJackVituli <rjvtechnology@gmail.com>
2022-04-14 22:08:57 +0300

    BSPM2022T15-17

--- Estudy_Project/accounts/models.py
+++ Estudy_Project/accounts/models.py
@@ -1,15 +1,31 @@
 from django.db import models
-# Create your models her
+from django.contrib.auth.models import User
 
-  
 
+# Create your models here.
 class Student(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-    password = models.CharField(max_length=50)
-    
-class Lecturer(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-    password = models.CharField(max_length=50)
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    First_Name = models.CharField('First Name', max_length=30, null=True, blank=True)
+    Last_Name = models.CharField('Last Name', max_length=30, null=True, blank=True)
+    Code = models.CharField('Code', max_length=30, null=True, blank=True)
+    ID_Number = models.CharField('Id Number', max_length=30, null=True, blank=True)
 
+    class Meta:
+        verbose_name_plural = "Students"
+
+    def __str__(self):
+        return str(self.user)
+
+class Lecturer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    First_Name = models.CharField('First Name', max_length=30, null=True, blank=True)
+    Last_Name = models.CharField('Last Name', max_length=30, null=True, blank=True)
+    Courses = models.CharField('Courses', max_length=30, null=True, blank=True)
+    ID_Number = models.CharField('Id Number', max_length=30, null=True, blank=True)
+    class Meta:
+        verbose_name_plural = "Lecturers"
+
+    def __str__(self):
+        return str(self.user)
