796977b39c8d4e0ae63e53e341437413cb153d6e
Roni Jack Vituli <100803621+RoniJack@users.noreply.github.com>
2022-04-26 20:32:43 +0300

    Update urls.py

--- DetailsAccount/urls.py
+++ DetailsAccount/urls.py
@@ -6,7 +6,6 @@ app_name = 'Details'
 
 urlpatterns = [
     path('detailsStudent/<student_id>', views.DetailsStudentView.as_view(), name='d-s'),
-    path('detailsLecturer/<lecturer_id>', views.DetailsLectureView.as_view(), name='d-l'), 
     path('Delete/<user_id>',views.deleteUser,name='delete-user'),
     path('changePassword/<user_id>',views.ChangeStudentPass.as_view(),name='ch-pass'),
     
