a6e96dd75d1b54885e1de3839c8e9631f89a0529
Roni Jack Vituli <100803621+RoniJack@users.noreply.github.com>
2022-04-26 20:32:09 +0300

    Update views.py

--- DetailsAccount/views.py
+++ DetailsAccount/views.py
@@ -65,45 +65,6 @@ class ChangeStudentPass(View):
           return render(request,"changePass.html",{'form_user': form})
 
 
-# ----------------------------------------------------------------------------------------------------------------------------------------------------
-
-class DetailsLectureView(View):
-  def get(self,request, lecturer_id):
-    lecturer = Lecturer.objects.get(pk=lecturer_id)
-    userLecturer = User.objects.get(pk=lecturer_id)
-    form_lecturer = LecturerForm(request.POST or None, instance=lecturer)
-    form_user = UserUpdate(None,instance=userLecturer)
-    return render(request, "details.html",{'form':form_lecturer, 'form_user':form_user})
-  
-  def post(self,request,lecturer_id):
-    form_lecturer = LecturerForm(request.POST, request.FILES)
-    if form_lecturer.is_valid():
-        first_name = form_lecturer.cleaned_data['first_name']
-        last_name = form_lecturer.cleaned_data['last_name']
-        user = User.objects.get(pk=lecturer_id)
-        email = form_lecturer.cleaned_data['email']
-        user.first_name = first_name
-        user.last_name = last_name
-        user.email = email
-        img = request.FILES.get('imag_profile')
-        if img != None:
-          user.imag_profile = img
-        user.save()
-        lecturer = Lecturer.objects.get(pk=lecturer_id)
-        lecturer.first_name = first_name
-        lecturer.last_name = last_name
-        lecturer.Phone = form_lecturer.cleaned_data['Phone']
-        lecturer.ID = form_lecturer.cleaned_data['ID']
-        lecturer.save()
-        return redirect('HomePage:home')
-    userLecturer = User.objects.get(pk=lecturer_id)
-    form_user = UserUpdate(None,instance=userLecturer)   
-    return render(request,'details.html',{'form':form_lecturer, 'form_user':form_user})  
-
-
-
-
-
 def deleteUser(request,user_id):
   user = User.objects.get(pk=user_id)
   user.delete()
