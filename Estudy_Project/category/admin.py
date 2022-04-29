7c77c130d86bbf5bc8e4dde05488c021de53670d
Dolev Peretz <73190393+DolevPeretz@users.noreply.github.com>
2022-04-29 16:31:10 +0300

    Delete Estudy_Project/category directory

--- Estudy_Project/category/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from category.models import Course,HomeWork, YouTubeVideo
-
-# Register your models here.
-
-
-admin.site.register(Course)
-admin.site.register(HomeWork)
-admin.site.register(YouTubeVideo)
