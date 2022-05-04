960d25be3f97dcc17c136db76044a814c8d1dd82
Dolev Peretz <73190393+DolevPeretz@users.noreply.github.com>
2022-05-04 11:23:26 +0300

    Merge + tests

--- Estudy_Project/HomePage/form.py
+++ Estudy_Project/HomePage/form.py
@@ -4,16 +4,24 @@ from accounts.models import User
 
 
 class PostForm(forms.ModelForm):
-  class Meta:
-    model = Post
-    fields = ('title','description')
-    widgets = {
-      'title': forms.TextInput(attrs={'class':'form-control border-0'}),
-      'description': forms.Textarea(attrs={'name':'message' ,'id':'msgus' ,'cols':'10' ,'rows':'5' ,'class':'form-control bg-light'}),
-    }
+    class Meta:
+        model = Post
+        fields = ("title", "description")
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control border-0"}),
+            "description": forms.Textarea(
+                attrs={
+                    "name": "message",
+                    "id": "msgus",
+                    "cols": "10",
+                    "rows": "5",
+                    "class": "form-control bg-light",
+                }
+            ),
+        }
 
 
 class UserForm(forms.ModelForm):
-  class Meta:
-    model = User
-    fields = ('permissions',)
+    class Meta:
+        model = User
+        fields = ("permissions",)
--- Estudy_Project/HomePage/models.py
+++ Estudy_Project/HomePage/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 
 # Create your models here.
@@ -8,4 +7,3 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     date = models.DateField(auto_now=True, auto_now_add=False)
     description = models.TextField(max_length=100, null=True)
-
