135b5fd55d54b01b0226d7cc77d0198f0cd560e6
Roni Jack Vituli <100803621+RoniJack@users.noreply.github.com>
2022-05-11 11:53:57 +0300

    Create form.py

---
+++ Estudy_Project/forum/form.py
@@ -0,0 +1,19 @@
+import django
+
+from django import forms
+from forum.models import Post
+
+
+class PostForumForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ("message",)
+        widgets = {
+            "message": forms.Textarea(
+                attrs={
+                    "placeholder": "What's on your mind, Estudy?",
+                    "spellcheck": "false",
+                    "style": "width: 449px; height: 120px;",
+                }
+            )
+        }
