f34acdc2c015841e943c4453d7f8617b8ca0c134
Roni Jack Vituli <100803621+RoniJack@users.noreply.github.com>
2022-05-11 11:55:18 +0300

    Update views.py

--- Estudy_Project/forum/views.py
+++ Estudy_Project/forum/views.py
@@ -8,14 +8,10 @@ from accounts.models import User
 class forumView(View):
     def get(self, request):
         form_post = PostForumForm()
-        form_comment = CommentForm()
-        all_posts = Post.objects.prefetch_related("comment_set")
         return render(
             request,
             "fourm.html",
             {
                 "form_post": form_post,
-                "all_posts": all_posts,
-                "form_comment": form_comment,
             },
         )
