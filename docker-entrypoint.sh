1e4f77a527fc9cde9235313eb61fd3d53de77293
HaielDahan <73176023+HaielDahan@users.noreply.github.com>
2022-05-13 12:54:36 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,3 +10,14 @@ fi
 # Collect static files
 echo "Collect static files"
 python manage.py collectstatic --noinput
+
+python manage.py makemigrations
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+
+python manage.py clearsessions
+# Start server
+echo "Starting server on PORT:$PORT"
+python manage.py runserver 0.0.0.0:$PORT
