a65cf3558500857b8d716ed622d3aef084bc21e4
Roni Jack Vituli <100803621+RoniJack@users.noreply.github.com>
2022-05-15 18:46:32 +0300

    Update views.py

--- forum/views.py
+++ forum/views.py
@@ -35,13 +35,7 @@ class forumView(View):
             post.save()
         return redirect("Forum:forum-main")
 
-<<<<<<< HEAD
-
     
-=======
-   
-   
->>>>>>> cf1ceec (fix)
 def PostComments(request, user_id, post_id):
     if request.method == "POST":
         form = CommentForm(request.POST)
