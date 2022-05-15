a1a2d68aa2beccd1cb6701a74c17693e51a80dcd
Yaki93 <73586527+Yaki93@users.noreply.github.com>
2022-05-15 11:39:50 +0300

    BSPM2022T15-112

--- forum/urls.py
+++ forum/urls.py
@@ -6,5 +6,6 @@ app_name = "Forum"
 urlpatterns = [
     path("", views.forumView.as_view(), name="forum-main"),
     path("<user_id>", views.forumView.as_view(), name="post-post"),
+    path("<user_id>/<post_id>", views.PostComments, name="comment-post"),
 ]
 
