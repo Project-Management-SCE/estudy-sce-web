7b850e8ef2a4357a2740ccad6e58b546d2e0da85
Roni Jack Vituli <100803621+RoniJack@users.noreply.github.com>
2022-05-15 11:42:47 +0300

    Update urls.py

--- forum/urls.py
+++ forum/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("", views.forumView.as_view(), name="forum-main"),
     path("<user_id>", views.forumView.as_view(), name="post-post"),
     path("<user_id>/<post_id>", views.PostComments, name="comment-post"),
+    path("delete/<int:post_id>/", views.DeletePost, name="delete-po"),
+    path("delComment/<int:comment_id>/", views.DeleteComment, name="delete-co"),
 ]
 
