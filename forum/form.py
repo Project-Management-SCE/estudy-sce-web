fb7fdf71b52941accb0a41818dc53e9fd920d4d7
Yaki93 <73586527+Yaki93@users.noreply.github.com>
2022-05-18 11:34:39 +0300

    BSPM2022T15-112

--- forum/form.py
+++ forum/form.py
@@ -17,3 +17,18 @@ class PostForumForm(forms.ModelForm):
                 }
             )
         }
+
+        
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ("message",)
+        widgets = {
+            "message": forms.Textarea(
+                attrs={
+                    "class": "form-control ml-1 shadow-none textarea",
+                    "style": "height: 90px;",
+                }
+            )
+        }
