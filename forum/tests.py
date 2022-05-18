c3fcfa050cdf262329605e29fece59daf4daac54
Yaki93 <73586527+Yaki93@users.noreply.github.com>
2022-05-18 11:36:05 +0300

    BSPM2022T15-123

--- forum/tests.py
+++ forum/tests.py
@@ -38,3 +38,48 @@ class PostTestCase(TestCase):
         )
         # Assert
         self.assertEqual(response.status_code, 200)
+
+        
+        
+ class PostCommentTestCase(TestCase):
+    def setUp(self):
+        self.credentials = {
+            "username": "lectureruser",
+            "password": "5t4r3e2w1q",
+            "is_lecturer": True,
+        }
+        
+        self.user = User.objects.create_user(**self.credentials)
+        self.post = Post.objects.create(user=self.user,title="title",message="message",date=datetime.today())
+    @tag("unit-test")
+    def testCommentView(self):
+        form = {"message": "contant!"}
+        response = self.client.post(
+            reverse("Forum:comment-post", kwargs={"user_id": self.user.id,"post_id":self.post.id}),
+            data=form,
+            follow=True,
+        )
+        # Assert
+        self.assertEqual(response.status_code, 200)
+
+    @tag("integration-test")
+    def test_create_and_delete(self):
+        response = self.client.post("/acoounts/login/", self.credentials, follow=True)
+        self.assertTrue(response.context["user"].is_lecturer)
+        form = {"message": "contant!"}
+        response = self.client.post(
+            reverse("Forum:comment-post", kwargs={"user_id": self.user.id,"post_id":self.post.id}),
+            data=form,
+            follow=True,
+        )
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, reverse("Forum:forum-main"))
+        
+        response = self.client.post(
+            reverse("Forum:delete-po", kwargs={"post_id":self.post.id}),
+            data=form,
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, reverse("Forum:forum-main"))
