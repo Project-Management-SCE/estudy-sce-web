396915d6f847d78faa53fb4083986468f3cd0c0c
Dolev Peretz <73190393+DolevPeretz@users.noreply.github.com>
2022-05-19 11:08:43 +0300

    BSPM2022T15-103

--- category/urls.py
+++ category/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('<user_id>', views.CategoryView.as_view(), name="cat"),
     path('course/<course_id>/<user_id>',views.HomeWorksView.as_view(), name="homework"),
     path('upload-file/<course_id>/<user_id>',views.UploadFileView.as_view(), name="u-f"),
+    path("upload/", views.CreateCourseView.as_view(), name="create-course"),
     path("del/<course_id>", views.deleteCourse, name="delete-course"),
     path('delfile/<course_id>/<hw_id>',views.deleteFile ,name="delete-file"),
 ]
