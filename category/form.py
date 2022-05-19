ca0fb49b67aaa390acca85a1917b1913ae20f28b
Dolev Peretz <73190393+DolevPeretz@users.noreply.github.com>
2022-05-19 11:07:50 +0300

    BSPM2022T15-103

--- category/form.py
+++ category/form.py
@@ -8,8 +8,26 @@ class CourseForm(forms.ModelForm):
     model = Course
     fields = ('department','year','semester','kind_of')
 
+    
 
 class HomeWorkForm(forms.ModelForm):
   class Meta:
     model = HomeWork
     fields = ('nameFile','file','course')
+    
+    
+    
+    
+    
+class CreatCourseForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ("department", "year", "semester", "name_course", "kind_of")
+        widgets = {
+            "name_course": forms.TextInput(
+                attrs={
+                    "type": "text",
+                    "class": "bg-light form-control",
+                }
+            ),
+        }
