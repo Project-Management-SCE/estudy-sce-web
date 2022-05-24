abf8f4bb04598622847149d8f8e1654e6361e1ff
HaielDahan <73176023+HaielDahan@users.noreply.github.com>
2022-05-24 08:37:42 +0300

    BSPM2022T15-138

--- category/views.py
+++ category/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import redirect, render
 from django.views import View
 from category.models import HomeWork, YouTubeVideo
-from category.form import CourseForm, HomeWorkForm, CreatCourseForm
-from category.models import Course 
-from accounts.models import Student, User 
-# Create your views here.
-
-
+from category.form import CourseForm, HomeWorkForm, CreatCourseForm, CommentHomeWorkForm
+from category.models import Course, CommentHomeWork
+from accounts.models import Student, User
 
 class CategoryView(View):
   def get(self,request,user_id):
@@ -18,3 +15,22 @@ class CategoryView(View):
     return render(request,"category.html",{'form':form})
   def post(self,request):
     pass
+  
+  
+  
+  
+  
+  class ForumFileView(View):
+    """
+    ForumFileView
+    """
+
+    def get(self, request, hw_id):
+        hw = HomeWork.objects.get(pk=hw_id)
+        comments = CommentHomeWork.objects.filter(hw=hw).iterator()
+        comment = CommentHomeWorkForm()
+        return render(
+            request,
+            "ForumFile.html",
+            {"hw": hw, "comments": comments, "form_comment": comment},
+        )
