2b42611b9c8f4417c0ea7858f6128b5aefbc6896
HaielDahan <73176023+HaielDahan@users.noreply.github.com>
2022-05-26 10:37:51 +0300

    BSPM2022T15-143

--- YouTube/tests.py
+++ YouTube/tests.py
@@ -3,7 +3,7 @@ from django.urls import reverse,resolve
 from YouTube.views import SearchVideoView
 # Create your tests here.
 
-@tag('y')
+@tag('unit-test')
 class YouTubeTest(TestCase):
 
   def test_url(self):
@@ -24,3 +24,25 @@ class YouTubeTest(TestCase):
     search = "python"
     response = self.client.post(reverse("YouTube:youtube"),data={'search':search})
     self.assertNotEquals(response.context['videos'],None)
+
+    
+    
+  @tag("integration-test")
+  def test_create_and_delete(self):
+      response = self.client.post("/acoounts/login/", self.credentials, follow=True)
+      self.assertTrue(response.context["user"].is_lecturer)
+      
+      search = "c++"
+      response = self.client.post(reverse("YouTube:youtube"), data={'search':search})
+      id_video = response.context['videos'][0]['index']
+      self.assertNotEquals(response.context['videos'],None)
+    
+      form_da = {"department":"1", "year":"1", "semester":"1","kind_of":"5"}
+      response = self.client.post(reverse("YouTube:save-video", kwargs={'video_index':id_video}), data=form_da)
+
+      youtube = YouTubeVideo.objects.create(id = 3874783, user=self.user, course=self.course, channelId="channelId", IdVideo="test",profile="profile",title="title",thumbnails='thumbnails')
+
+      response = self.client.get(reverse('Category:delete-video',kwargs={'course_id':self.course.id,'video_id':youtube.id}))
+
+      self.assertEqual(response.status_code,302)
+      self.assertRedirects(response, reverse("Category:videos",kwargs={'course_id':self.course.id}))
