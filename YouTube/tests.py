85ea05cf5ae1cda7ccabce6d1e458a40fa5fc642
Roni Jack Vituli <100803621+RoniJack@users.noreply.github.com>
2022-05-28 11:49:10 +0300

    fix - tests

--- YouTube/tests.py
+++ YouTube/tests.py
@@ -1,33 +1,46 @@
+from http.client import responses
 from django.test import TestCase, tag
-from django.urls import reverse, resolve
+from django.urls import reverse,resolve
+from accounts.models  import User
+from category.models  import Course, YouTubeVideo
 from YouTube.views import SearchVideoView
-
 # Create your tests here.
 
-
-@tag("unit-test")
+@tag('unit-test')
 class YouTubeTest(TestCase):
-    def test_url(self):
-        url = reverse("YouTube:youtube")
-        self.assertEqual(resolve(url).func.view_class, SearchVideoView)
 
-    def test_get(self):
-        response = self.client.get(reverse("YouTube:youtube"))
-        self.assertEqual(response.status_code, 200)
+  def setUp(self):
+      self.credentials = {
+            "username": "lectureruser",
+            "password": "5t4r3e2w1q",
+            "is_lecturer": True,
+        }
 
-    def test_post(self):
-        search = "c++"
-        response = self.client.post(reverse("YouTube:youtube"), data={"search": search})
-        self.assertEqual(response.status_code, 200)
+      self.user = User.objects.create_user(**self.credentials)
+      self.course = Course.objects.create(name_course="Test",kind_of="5")
+  def test_url(self):
+    url = reverse("YouTube:youtube")
+    self.assertEqual(resolve(url).func.view_class, SearchVideoView)
 
-    def test_post_return(self):
-        search = "python"
-        response = self.client.post(reverse("YouTube:youtube"), data={"search": search})
-        self.assertNotEquals(response.context["videos"], None)
-  
-  
-    @tag("integration-test")
-    def test_create_and_delete(self):
+  def test_get(self):
+    response = self.client.get(reverse("YouTube:youtube"))
+    self.assertEqual(response.status_code, 200)
+
+
+  def test_post(self):
+    search = "c++"
+    response = self.client.post(reverse("YouTube:youtube"),data={'search':search})
+    self.assertEqual(response.status_code, 200)
+
+  def test_post_return(self):
+    search = "python"
+    response = self.client.post(reverse("YouTube:youtube"),data={'search':search})
+    self.assertNotEquals(response.context['videos'],None)
+
+
+
+  @tag("integration-test")
+  def test_create_and_delete(self):
       response = self.client.post("/acoounts/login/", self.credentials, follow=True)
       self.assertTrue(response.context["user"].is_lecturer)
       
@@ -45,4 +58,9 @@ class YouTubeTest(TestCase):
 
       self.assertEqual(response.status_code,302)
       self.assertRedirects(response, reverse("Category:videos",kwargs={'course_id':self.course.id}))
+
+
+
+
+
 
